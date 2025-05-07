89e28b039b8220fc7933989a542052cdd306043f
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2025-05-07 18:30:05 +0600

    sending historical low notification working

--- UtilityFunctions/priceChecker.js
+++ UtilityFunctions/priceChecker.js
@@ -20,9 +20,22 @@ const checkPrice = async (client) => {
             const gameData = gameResponseData[game.appid].data
 
             const gamePriceInfo = gameData.price_overview
-            if(historyDetails.lows[0].cut <= gamePriceInfo.discount_percent){
-                console.log(`${game.name} is in historical sale now`)
-                notifyUsers(client, game.guildId)
+            if(gamePriceInfo){
+                if(gamePriceInfo.discount_percent > 0){
+                    if(historyDetails.lows[0].cut <= gamePriceInfo.discount_percent){
+                        console.log(`${game.name} is in historical sale now`)
+                        let data = {
+                            client,
+                            guildId: game.guildId,
+                            appid: game.appid,
+                            gameData
+                        }
+                        notifyUsers(data, sendHistoricalLowEmbed)
+                    }
+                    else {
+                        console.log('Not in historical low but in sale')
+                    }
+                }
             }
         })
     } catch (error) {
@@ -35,11 +48,34 @@ const checkPrice = async (client) => {
  * send notification to users
  * @param {Client} client 
  */
-const notifyUsers = async (client, guildId) => {
-    const guildSettings = await GuildSettings.findOne({guildId})
+const notifyUsers = async (data, callBack) => {
+    try {
+        const {client, guildId, appid, gameData} = data
+        gameData.appid = appid
+        const guildSettings = await GuildSettings.findOne({guildId})
 
-    const channel = await client.channels.fetch(guildSettings.notificationChannelId).catch(() => null);
-    if (channel) console.log('It is working');
+        const channel = await client.channels.fetch(guildSettings.notificationChannelId).catch(() => null);
+        if (!channel) {
+            throw new Error('❌ Set a channel to receive updates. Command: /setchannel')
+        }
+        
+        callBack(channel, gameData)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const sendHistoricalLowEmbed = (channel, game) => {
+    const embed = new EmbedBuilder()
+                    .setTitle(`💸 ${game.name} is at historical low!`)
+                    .setDescription(`\n**Price:**~~$${game.price_overview.initial_formatted}~~ → **$${game.price_overview.final_formatted}**\n
+                                    **Discount:** ${game.price_overview.discount_percent}%\n
+                                    [Open in Steam](https://store.steampowered.com/app/${game.appid}/)`)
+                    .setImage(game.header_image)
+                    .setColor(0x1b2838)
+    channel.send({
+        embeds: [embed]
+    })
 }
 
 module.exports = {
